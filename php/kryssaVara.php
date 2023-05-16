fbdce23dac5a50d1d0178295384a9fe3a45f30bd
Aron <47663@gymnasium.ax>
2023-05-16 11:38:43 +0300

    Toggla checked värde för vara och grundläggande tester funkar

--- php/kryssaVara.php
+++ php/kryssaVara.php
@@ -2,11 +2,36 @@
 declare (strict_types=1);
 
 // Läs in gemensamma funktioner
+require_once "funktioner.php";
 
 // Läs och kontrollera indata
+// Rätt metod
+if ($_SERVER['REQUEST_METHOD']!=='POST'){
+    $error=new stdClass();
+    $error->meddelande=["wrong method", "sidan ska anropas med POST"];
+    skickaJSON($error, 405);
+}
 
+// Kontrollera id
+$id=filter_input(INPUT_POST, "id", FILTER_VALIDATE_INT);
+if(!isset($id) || $id<1){
+    $error=new stdClass();
+    $error->meddelande=["Bad request","'id' saknas eller är ogiltigt"];
+    skickaJSON($error, 400);
+}
 // Koppla mot databasen
+$db=connectDb();
 
 // Toggla checked-värdet
+$sql="UPDATE varor SET checked=NOT(checked) WHERE id=:id";
+$stmt=$db->prepare($sql);
+$stmt->execute(['id'=>$id]);
+
+if($stmt->rowCount()===0) {
+    $error=new stdClass();
+    $error->meddelande=["Bad request", "Kunde inte uppdatera varan"];
+    skickaJSON($error, 400);
+}
 
 // Skicka svar
+skickaJSON(['meddelande'=>'OK']);
