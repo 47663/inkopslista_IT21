cb08b7194d5dd689dac2459285eb0554ccd6bd1f
Aron <47663@gymnasium.ax>
2023-05-16 11:08:25 +0300

    Spara vara och grundläggande tester funkar

--- php/HamtaAlla.php
+++ php/HamtaAlla.php
@@ -1,2 +1,20 @@
 <?php
 declare (strict_types=1);
+
+require_once "funktioner.php";
+
+// Koppla mot databasen
+$db = connectDb();
+
+// Hämta data
+$sql="SELECT id, name, checked FROM varor";
+$stmt=$db->query($sql);
+
+$rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+$resultat=[];
+foreach($rows as $post) {
+    $resultat[]=$post;
+}
+
+// Skicka svar
+skickaJSON($resultat);
--- php/sparaVara.php
+++ php/sparaVara.php
@@ -1,2 +1,33 @@
 <?php
 declare (strict_types=1);
+
+require_once "funktioner.php";
+//läs indata och sanera
+
+//kontrollera metod
+if ($_SERVER['REQUEST_METHOD']!=='POST'){
+    $error=new stdClass();
+    $error->meddelande=["wrong method", "sidan ska anropas med POST"];
+    skickaJSON($error, 405);
+}
+
+// läs indata
+$vara=filter_input(INPUT_POST, 'vara', FILTER_SANITIZE_SPECIAL_CHARS);
+if(!isset($vara) || mb_strlen($vara)>50){
+    $error=new stdClass();
+    $error->meddelande=["bad input", "Parametern 'vara' saknas eller är mer än 50 tecken"];
+    skickaJSON($error, 400);
+}
+
+//koppla mot databas
+$db=connectDB();
+
+//spara data
+$sql="INSERT INTO varor (namn) VALUES (:vara)";
+$stmt=$db->prepare($sql);
+
+$stmt->execute(['vara'=>$vara]);
+$id=$db->lastInsertId();
+
+//skicka tillbaka svar
+skickaJSON(['id'=>$id]);
